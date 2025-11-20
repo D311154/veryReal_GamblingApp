36db8c17f19f219c13e10ad6af7cc60cda7de837
D311154 <d311154@edu.curio.nl>
2025-11-20 10:06:08 +0100

    initial commit

---
+++ GamblingApi/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GamblingApi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
